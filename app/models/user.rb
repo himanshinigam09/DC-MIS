4531ffe692f22b299275bda020b76225c3c59eef
amarpreetjethra <amyjethra@gmail.com>
2015-08-13 16:30:04 +0530

    Publication added to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-   attr_accessible :first_name, :last_name, :email, :password
+attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
    has_secure_password
    validates :email, uniqueness: true
-    validates :first_name, :last_name, :email, presence: true
-   validates :password, length: { in: 8..20 } 
+   validates :first_name, :last_name, :email, presence: true
+   validates :password, length: { in: 8..20 } , confirmation: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150811180411) do
+ActiveRecord::Schema.define(:version => 20150813092530) do
 
   create_table "achievements", :force => true do |t|
     t.string   "dc_member_id"
@@ -283,8 +283,9 @@ ActiveRecord::Schema.define(:version => 20150811180411) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.string   "password_confirmation"
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
   end
 
 end
